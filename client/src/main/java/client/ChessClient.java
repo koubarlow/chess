f2afaecd73381a38008f1b2bbe8f53c830e4a384
Kou Barlow <kougbarlow@gmail.com>
2025-11-11 17:48:24 -0700

    added escape sequences, state and began chess client

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,4 +1,40 @@
 package client;
 
+import server.ServerFacade;
+
+import static client.EscapeSequences.LOGO;
+
 public class ChessClient {
+
+    private String visitorName = null;
+    private final ServerFacade server;
+    private State state = State.SIGNEDOUT;
+
+    public ChessClient(String serverUrl) throws Exception {
+        server = new ServerFacade(serverUrl);
+    }
+
+    public void run() {
+        System.out.println(LOGO + "Welcome to chess. Sign in to start.");
+        System.out.println(help());
+
+
+    }
+
+    public String help() {
+        if (state == State.SIGNEDOUT) {
+            return """
+                    - login <username> <password>
+                    - register <username> <password> <email>
+                    - quit
+                    """;
+        }
+        return """
+                - create game
+                - list games
+                - join game
+                - logout
+                - quit
+                """;
+    }
 }
